2cac8104e9a55536a719faf09c9e40d4ee308cef
SANDRA DWI ASTUTI <151292658+sandradwiastuti@users.noreply.github.com>
2025-06-11 22:34:18 +0700

    inifooternyaa

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+</main> <footer class="text-center mt-5 mb-3">
+        <p>&copy; <?= date('Y') ?> Dompetku. All Rights Reserved.</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script src="../assets/js/main.js"></script>
+</body>
+</html>
